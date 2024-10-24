48bb518bcafc283fd60a44f7bfffe733109ea018
Akira Taguchi <akirahattivatti@gmail.com>
2024-10-24 14:37:49 +0300

    Finish week 43 thu

--- stage2.py
+++ stage2.py
@@ -10,33 +10,19 @@ scancode_jsons = [json.load(open('./scancode-licensedb/docs/' + f ,'r')) for f i
 for json_loader in scancode_jsons:
   try:
     spdx_license_key = json_loader['spdx_license_key']
-    if spdx_license_key in licenses.keys:
+    if spdx_license_key in licenses.keys():
       licenses[spdx_license_key] = json_loader['text']
   except:
     pass
 
-manual_filenames = [f.removesuffix('.txt') for f in listdir('manual-licenses/') if (re.compile('^.*\\.txt').match(f))]
-
-for license_key in manual_filenames:
-  with open('manual-licenses/' + license_key + '.txt', 'r') as file_object:
-    license_texts.append((license_key, file_object.read()))
-
-for license_key in manual_filenames:
-  manually_fetchable_licenses.remove(license_key)
+manual_txts = [f.removesuffix('.txt') for f in listdir('manual-licenses/') if (re.compile('^.*\\.txt').match(f))]
+for filename in manual_txts:
+  with open('manual-licenses/' + filename + '.txt', 'r') as file_object:
+    licenses[filename] = file_object.read()
 
 with open ('license-exceptions.json', 'r') as file_object:
   json_loader = json.load(file_object)
   for license_key in json_loader:
-    manually_fetchable_licenses.remove(license_key)
+    licenses.pop(license_key)
 
-with open('fetch_these_licenses_manually.txt', 'w') as file_object:
-  file_object.write('\n'.join(manually_fetchable_licenses))
-
-all_licenses = manual_filenames
-
-for license_pair_1 in license_texts:
-  for license_pair_2 in license_texts:
-    if license_pair_1[0] == license_pair_2[1]:
-      continue
-    if license_pair_1[1] == [license_pair_2[1]]:
-      print(license_pair_1[0] + ' is duplicates with ' + license_pair_2[0])
+print(len(licenses))
